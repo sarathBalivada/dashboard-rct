b473aad69ff3b54ba6f8c76a1da11ab278ab20b5
Sarath Balivada <sarath.balivada95@gmail.com>
2023-02-13 20:39:52 +0530

    navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
     <div className='flex justify-between items-center'>
      <Link to="/" onClick={() => { setActiveMenu(false) }} className="items-center gap-3 ml-3 mt-4 flex-container
      flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900" >
-       <SiShopware /> <span>Shoppy</span>
+       <SiShopware /> <span>ShoppyMart</span>
        </Link>
        <TooltipComponent content="Menu" 
        position="BottomCenter" >
